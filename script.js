d7f5171a871d1766837f554ff52d099fa405f47c
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-26 22:41:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+	const sampleTable = document.getElementById("sampleTable");
+	const newRow = document.createElement("tr");
+	newRow.innerHTML = `
+        <td>New Cell1</td>
+        <td>New Cell2</td>
+      `;
+	sampleTable.prepend(newRow, sampleTable.rows[0]);
   
 }
