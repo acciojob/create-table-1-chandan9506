4686cebba9c6031ad8d69429bb24ebe587b49623
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-26 22:51:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
+	 const firstRow = document.getElementById("sampleTable").rows[0];
+    
+    // If there is an existing first row, clear its content
+    if (firstRow) {
+        firstRow.cells[0].textContent = ""; // Clear content of the first cell
+        firstRow.cells[1].textContent = ""; // Clear content of the second cell
+    }
 	const sampleTable = document.getElementById("sampleTable");
 	const newRow = document.createElement("tr");
 	newRow.innerHTML = `
